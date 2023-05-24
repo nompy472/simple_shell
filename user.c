0c39810e39707ae12c301d986026a84fd2641d1c
Siphiwe <nompychiloane@gmail.com>
2023-05-24 02:32:26 -0700

    pompt

--- user.c
+++ user.c
@@ -10,12 +10,6 @@ void prompt_user(void)
 	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
 		flags.interactive = 1;
 	if (flags.interactive)
-	{
 		write(STDERR_FILENO, "cisfun$ ", 7);
-	}
-	else
-	{
-		write(STDERR_FILENO, "Not found", 8);
-	}
 
 }
