4d3a62fb00647f4fb2dea005bacb0afb3d617d8d
Siphiwe <nompychiloane@gmail.com>
2023-05-24 02:28:50 -0700

    Print error

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Simple shell
 
-Collaboration project team project by Nompay and Juliana
+Collaboration project team project by Nompy and Juliana
 Writing a simple UNIX command interpreter.
--- user.c
+++ user.c
@@ -10,5 +10,12 @@ void prompt_user(void)
 	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
 		flags.interactive = 1;
 	if (flags.interactive)
-		write(STDERR_FILENO, "cisfun$ ", 2);
+	{
+		write(STDERR_FILENO, "cisfun$ ", 7);
+	}
+	else
+	{
+		write(STDERR_FILENO, "Not found", 8);
+	}
+
 }
